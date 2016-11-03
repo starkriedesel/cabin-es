51c4797285a717d0a76aae96596f8f7bac29e618
Stark Riedesel <starkriedesel@gmail.com>
2016-11-03 14:40:08 -0500

    Properly default the type parameter

--- lib/cabin/outputs/elastic_search.rb
+++ lib/cabin/outputs/elastic_search.rb
@@ -4,11 +4,11 @@ require 'elasticsearch'
 class Cabin::Outputs::ElasticSearch
   attr_accessor :index, :type, :urls
 
-  def initialize(es_urls, index, type="cabin", logger=nil)
+  def initialize(es_urls, index, type=nil, logger=nil)
     es_urls = [es_urls] if es_urls.is_a? String
     self.urls = es_urls
     self.index = index
-    self.type = type
+    self.type = type || 'cabin'
     raise Exception.new 'Index must be not blank for Cabin::Outputs::ElasticSearch' if index.nil? or index =~ /^\s*$/
     raise Exception.new 'Type must be not blank for Cabin::Outputs::ElasticSearch' if type.nil? or type =~ /^\s*$/
     raise Exception.new 'At least one URL must be provided to Cabin::Outputs::ElasticSearch' if es_urls.nil? or es_urls.count == 0
