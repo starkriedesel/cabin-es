9b9d874c88bd5b4bcdd285930607c1482119f5a1
Stark Riedesel <starkriedesel@gmail.com>
2016-11-03 15:49:14 -0500

    Move sanity checks to method

--- lib/cabin/outputs/elastic_search.rb
+++ lib/cabin/outputs/elastic_search.rb
@@ -2,22 +2,27 @@ require 'cabin'
 require 'elasticsearch'
 
 class Cabin::Outputs::ElasticSearch
-  attr_accessor :index, :type, :urls
+  attr_accessor :settings, :es
 
-  def initialize(es_urls, index, type=nil, logger=nil)
-    es_urls = [es_urls] if es_urls.is_a? String
-    self.urls = es_urls
-    self.index = index
-    self.type = type || 'cabin'
-    raise Exception.new 'Index must be not blank for Cabin::Outputs::ElasticSearch' if self.index.nil? or self.index =~ /^\s*$/
-    raise Exception.new 'Type must be not blank for Cabin::Outputs::ElasticSearch' if self.type.nil? or self.type =~ /^\s*$/
-    raise Exception.new 'At least one URL must be provided to Cabin::Outputs::ElasticSearch' if es_urls.nil? or es_urls.count == 0
-    @es = Elasticsearch::Client.new urls: es_urls, logger: logger
+  def initialize(args={})
+    logger = args.delete 'logger'
+    self.settings = {index: 'cabin', type: 'cabin'}.merge(args)
+    sanity_check!
+    self.es = Elasticsearch::Client.new urls: settings[:urls], logger: logger
+  end
+
+  def sanity_check!
+    settings[:urls] ||= [settings.delete(:url)] if settings.key? :url
+    [:index, :type].each {|x| settings[x] = settings[x].to_s}
+    raise Exception.new 'Index must be not blank for Cabin::Outputs::ElasticSearch' if settings[:index] =~ /^\s*$/
+    raise Exception.new 'Type must be not blank for Cabin::Outputs::ElasticSearch' if settings[:type] =~ /^\s*$/
+    raise Exception.new 'At least one URL must be provided to Cabin::Outputs::ElasticSearch' if settings[:urls].nil? or settings[:urls].count == 0
   end
 
   def <<(event)
     type = event.delete :_type
-    type = self.type if type.nil?
-    @es.index index: self.index, type: type, body: event
+    type = event.delete "_type" if type.nil?
+    type = settings[:type] if type.nil?
+    es.index index: settings[:index], type: type.to_s, body: event
   end
 end
